b94ac0f76b5dc24d0ce94ad56110113a7a622de6
Soraia Tavares <soraiaoliveira094@outlook.com>
2023-11-12 02:11:40 +0100

    for -> htmlFor

--- app/lawyer/page.tsx
+++ app/lawyer/page.tsx
@@ -20,8 +20,8 @@ export default async function Lawyers({
                     id="exampleSearch2"
                     placeholder="Type query" />
                 <label
-                    for="exampleSearch2"
-                    class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                    htmlFor="exampleSearch2"
+                    className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
                     >Search...</label
                 >
             </div>
