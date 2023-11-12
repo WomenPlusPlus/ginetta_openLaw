6ea2710c547a757c29557d4daf514a79df707b36
Soraia Tavares <soraiaoliveira094@outlook.com>
2023-11-12 10:01:55 +0100

    fixed data types

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,11 +2,20 @@ export {}
 
 declare global {
     interface ILawyerCard {
-        name: string
-        specialty_areas: string[]
-        N_experience: number
-        language: string[]
-        Price_per_hour: number
-        review: number
+        id: number;
+        name: string;
+        language: string[];
+        employed_type: string;
+        specialty_areas: string[];
+        location_km: number;
+        price_per_hour: number;
+        winning_rate: number;
+        review: number;
+        provono: number;
+        n_experience: number;
+        earlist_available_within_n_day: number;
+        average_response_hour: number;
+        y: number;
+        scores: number;
     }
-  }
+}
